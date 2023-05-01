d48e0c8d3f2e4a8ccc3433a1d5a8d8b7f8a9ce8a
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-05-01 19:28:57 -0400

    consts, heroku port

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const allRoutes = require("./controllers");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
