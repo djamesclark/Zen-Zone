935612f56840611e3b25f0174333256411e7c882
djamesclark <djamesclark@gmail.com>
2023-05-04 18:53:05 -0400

    finished routing for the other pages

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoute = require('./homeRoute');
+const aboutusRoute = require('./aboutusRoute');
+const servicesRoute = require('./servicesRoute');
+const loginRoute = require('./loginRoute');
 
 router.use('/api', apiRoutes);
-router.use('/', homeRoute)
+router.use('/', homeRoute, aboutusRoute, servicesRoute, loginRoute)
 
 module.exports = router;
 
