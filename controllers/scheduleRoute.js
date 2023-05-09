837494457bf0e7919541c3262c8683d4e872af5a
djamesclark <djamesclark@gmail.com>
2023-05-09 10:56:06 -0400

    swapped code for scheduleRoute.js to properly route.

--- controllers/scheduleRoute.js
+++ controllers/scheduleRoute.js
@@ -1,13 +1,4 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoute = require('./homeRoute');
-const aboutusRoute = require('./aboutusRoute');
-const servicesRoute = require('./servicesRoute');
-const loginRoute = require('./loginRoute');
-const scheduleRoute = require('./scheduleRoute');
-
-router.use('/api', apiRoutes);
-router.use('./',homeRoute, aboutusRoute, servicesRoute, loginRoute, scheduleRoute);
 router.get('/schedule', (req, res)=> {
     res.render('schedule')
 })
